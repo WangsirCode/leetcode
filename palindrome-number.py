cd98bdde1dd18a110ac111ef1d4e410ae5560bce
Wangsir <781952273@qq.com>
2017-10-09 12:31:41 +0800

    add complexity

--- palindrome-number.py
+++ palindrome-number.py
@@ -1,13 +1,17 @@
 # Determine whether an integer is a palindrome. Do this without extra space.
+# Time:  O(n)
+# Space: O(1)
 class Solution(object):
     def isPalindrome(self, x):
         """
         :type x: int
         :rtype: bool
         """
+        if x < 0:
+            return False
         reversed = self.reverse(x)
         return reversed == x
-    
+
     def reverse(self, x):
         """
         :type x: int
