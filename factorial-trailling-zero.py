c5a766e0373bc739a88a2705d4648ad16bebaca4
Wangsir <781952273@qq.com>
2017-10-14 09:07:10 +0800

    trailling zeros

---
+++ factorial-trailling-zero.py
@@ -0,0 +1,18 @@
+# # Given an integer n, return the number of trailing zeroes in n!.
+
+# # Note: Your solution should be in logarithmic time complexity.
+# This question is pretty straightforward.
+
+# Because all trailing 0 is from factors 5 * 2.
+
+# But sometimes one number may have several 5 factors, for example, 25 have two 5 factors, 125 have three 5 factors. In the n! operation, factors 2 is always ample. So we just count how many 5 factors in all number from 1 to n.
+class Solution(object):
+    def trailingZeroes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return 0 if n == 0 else n / 5 + self.trailingZeroes(n / 5)
+
+if __name__ == "__main__":
+    print(Solution().trailingZeroes(1))
