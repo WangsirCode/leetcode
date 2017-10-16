14578c4eac59fc4a71924c6c179d479aee07b5a3
Wangsir <781952273@qq.com>
2017-10-16 08:40:54 +0800

    perfect number

---
+++ perfect-number.py
@@ -0,0 +1,32 @@
+# We define the Perfect Number is a positive integer that is equal to the sum of all its positive divisors except itself.
+
+# Now, given an integer n, write a function that returns true when it is a perfect number and false when it is not.
+# Example:
+# Input: 28
+# Output: True
+# Explanation: 28 = 1 + 2 + 4 + 7 + 14
+# Note: The input number n will not exceed 100,000,000. (1e8)
+
+class Solution(object):
+    def checkPerfectNumber(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num < 0:
+            return False
+        if num == 1:
+            return False
+        sum = 1 
+        i = 2
+        while i < num:
+            if num % i == 0:
+                sum += i
+            i += 1
+        return sum == num
+
+
+if __name__ == "__main__":
+    print(Solution().checkPerfectNumber(28))
+    print(Solution().checkPerfectNumber(10))
+    print(Solution().checkPerfectNumber(20))
