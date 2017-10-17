1620a703d75ae830f47ac601fe12ab5b7b238116
Wangsir <781952273@qq.com>
2017-10-17 10:41:01 +0800

    Minimum Index Sum of Two Lists

---
+++ minimux-index-sum-of-two-lists.py
@@ -0,0 +1,31 @@
+# Suppose Andy and Doris want to choose a restaurant for dinner, and they both have a list of favorite restaurants represented by strings.
+
+# You need to help them find out their common interest with the least list index sum. If there is a choice tie between answers, output all of them with no order requirement. You could assume there always exists an answer.
+
+# Example 1:
+# Input:
+# ["Shogun", "Tapioca Express", "Burger King", "KFC"]
+# ["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"]
+# Output: ["Shogun"]
+# Explanation: The only restaurant they both like is "Shogun".
+# Example 2:
+# Input:
+# ["Shogun", "Tapioca Express", "Burger King", "KFC"]
+# ["KFC", "Shogun", "Burger King"]
+# Output: ["Shogun"]
+# Explanation: The restaurant they both like and have the least index sum is "Shogun" with index sum 1 (0+1).
+
+class Solution(object):
+    def findRestaurant(self, A, B):
+        Aindex = {u: i for i, u in enumerate(A)}
+        best, ans = 1e9, []
+
+        for j, v in enumerate(B):
+            i = Aindex.get(v, 1e9)
+            if i + j < best:
+                best = i + j
+                ans = [v]
+            elif i + j == best:
+                ans.append(v)
+        return ans
+    
