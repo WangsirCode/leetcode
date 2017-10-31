e149495ce2da31ad9e1a230373eb7f91739cb26d
Wangsir <781952273@qq.com>
2017-10-31 19:40:17 +0800

    permutatuin

---
+++ permutation-ii.py
@@ -0,0 +1,26 @@
+# Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+
+# For example,
+# [1,1,2] have the following unique permutations:
+# [
+#   [1,1,2],
+#   [1,2,1],
+#   [2,1,1]
+# ]
+class Solution:
+    # @param num, a list of integer
+    # @return a list of lists of integers
+    def permuteUnique(self, nums):
+        solutions = [[]]
+        
+        for num in nums:
+            next = []
+            for solution in solutions:
+                for i in xrange(len(solution) + 1):
+                    candidate = solution[:i] + [num] + solution[i:]
+                    if candidate not in next:
+                        next.append(candidate)
+                
+            solutions = next 
+            
+        return solutions
