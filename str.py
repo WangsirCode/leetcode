725942b4a20e76cb8758f8a60c878d6e56c649e2
Wangsir <781952273@qq.com>
2017-11-24 17:15:08 +0800

    detectCapitalUse

--- str.py
+++ str.py
@@ -2,7 +2,7 @@ class Solution(object):
     def countBinarySubstrings(self, s):
         s = map(len, s.replace('01', '0 1').replace('10', '1 0').split())
         return sum(min(a, b) for a, b in zip(s, s[1:]))
-    
+
     def repeatedStringMatch(self, A, B):
         """
         :type A: str
@@ -21,3 +21,13 @@ class Solution(object):
                 result += 1
         
         return -1
+    
+    def detectCapitalUse(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+        isLoewr = word.lower() == word
+        isuppper = word.upper() == word
+        firstCapital = (word.lower()[1:] == word[1:] and word.upper()[0] == word[0])
+        return isLoewr or isuppper or firstCapital
