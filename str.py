e425eb1ff40790ddea8d41725e03a050737db8fe
Wangsir <781952273@qq.com>
2017-11-24 18:13:01 +0800

    Valid Parenthesis String

--- str.py
+++ str.py
@@ -21,7 +21,7 @@ class Solution(object):
                 result += 1
         
         return -1
-    
+
     def detectCapitalUse(self, word):
         """
         :type word: str
@@ -31,3 +31,41 @@ class Solution(object):
         isuppper = word.upper() == word
         firstCapital = (word.lower()[1:] == word[1:] and word.upper()[0] == word[0])
         return isLoewr or isuppper or firstCapital
+    def checkValidString(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        left = []
+        star = []
+        if not s:
+            return True
+        for index, value in enumerate(s):
+            if value == '(':
+                left.append(index)
+            elif value == '*':
+                star.append(index)
+            else:
+                if left:
+                    left.pop()
+                elif star:
+                    star.pop()
+                else:
+                    return False
+        
+        if not left:
+            return True
+        elif len(left) > len(star):
+            return False
+        else:
+            index = -1
+            for i in left:
+                index +=1
+                if index == len(star):
+                    return False
+                while(star[index] < i) :
+                    index += 1
+                    if index == len(star):
+                        return False
+                
+            return True
